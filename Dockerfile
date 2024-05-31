d292d9c929567d83d52fdcba1fc6769f4472a931
Tobia Tommasini <tobia.tommasini@p4i.it>
2024-05-31 10:35:57 +0200

    added debugging

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,8 @@ EXPOSE 80
 
 # Define environment variable
 ENV FLASK_APP=app.py
+ENV FLASK_ENV=development
+
 
 # Run app.py when the container launches
 CMD ["flask", "run", "--host", "0.0.0.0", "--port", "80"]
--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+import logging
+
+from flask import Flask, jsonify
 
 from src.make_bucket import push_to_container, read_blob_from_azure_to_dataframe
 from src.plotting import import_and_save_iris, plot_iris_dataset_gui
 
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+# Add this to your Flask app
+app.logger.setLevel(logging.DEBUG)
 
 
 def save_iris_and_upload():
@@ -17,6 +23,11 @@ def hello_world():
     df = read_blob_from_azure_to_dataframe()
     rend = plot_iris_dataset_gui(df)
     return rend
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify(error=str(error)), 500
 
 
 if __name__ == '__main__':
