4e4b90f399f75af14a8556e1b6e5cdf0deaef836
Tobia Tommasini <tobia.tommasini@p4i.it>
2024-05-31 15:40:55 +0200

    minor changes

--- tests/test_make_bucket.py
+++ tests/test_make_bucket.py
@@ -1,10 +1,20 @@
 import os
+import logging
+
 
 from dotenv import find_dotenv, load_dotenv
 from src.make_bucket import list_all_blobs
 from src.constants import BLOB_NAME
 
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+logger.addHandler(console_handler)
 _ = load_dotenv(find_dotenv())
+
 
 def test_az_key_in_env():
     az_key = os.environ.get("AZ_ACCESS_KEY", "not found")
@@ -17,5 +27,5 @@ def test_blob_in_bucket():
         assert BLOB_NAME in list_all_blobs()
 
     except AssertionError as e:
-        print(os.environ.get("AZ_ACCESS_KEY", "not found"))
+        logging.info(os.environ.get("AZ_ACCESS_KEY", "not found"))
         raise e
